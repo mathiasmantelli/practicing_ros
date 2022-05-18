f286d1fb90f1b9374f31beba55706a159e97fa74
Mathias Mantelli <mathiasfassini@gmail.com>
2022-05-18 17:58:35 -0300

    First commit.

---
+++ src/kalman_filter_task/src/kalman_filter_node.cpp
@@ -0,0 +1,18 @@
+#include "ros/ros.h"
+#include "geometry_msgs/Twist.h"
+#include "nav_msgs/Odometry.h"
+#include <stdio.h>
+
+void receive_odom_callback(const nav_msgs::Odometry::ConstPtr& msg){
+    ROS_INFO("Seq: [%d]", msg->header.seq);
+}
+
+int main(int argc, char **argv){
+
+    ros::init(argc, argv, "kalman_filter_node");
+    ros::NodeHandle my_node; 
+    std::cout << "TESTING" << std::endl;
+    ros::Subscriber pose_subscriber = my_node.subscribe("/odom",10, receive_odom_callback);
+    ros::spin();
+    return 0;
+}
